5216109f3e385d205364bc25c9947e9cc089518b
starcantfly <starcantfly@aliyun.com>
2022-02-22 14:29:03 +0800

    modify demo files

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping("/getMsg")
+    public String getMsg() {
+        return "this is test msg";
+    }
+
 }
